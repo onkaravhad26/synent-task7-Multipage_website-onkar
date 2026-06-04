6f913a1bee4881868e5a0eb793d6ab10606eef14
onkar avhad <onkaravhad26@gmail.com>
2026-06-04 13:52:57 +0530

    styling

---
+++ script.js
@@ -0,0 +1,20 @@
+function showMessage() {
+    alert("Ticket Booking Coming Soon!");
+}
+
+
+function bookTicket() {
+    alert("Ticket Booking Coming Soon!");
+}
+
+function openPlayStore() {
+    window.open("https://play.google.com", "_blank");
+}
+
+function openAppStore() {
+    window.open("https://www.apple.com/app-store/", "_blank");
+}
+
+function Message() {
+    alert("Message Sent! We will get back to you soon.");
+}
